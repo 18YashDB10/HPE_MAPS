68a6a76554e958ee4f1ee26dd490c4251f2b73df
Yash DB <vvce22cse0119@vvce.ac.in>
2025-05-08 18:17:05 +0530

    Updates for dynamic updation

--- app.js
+++ app.js
@@ -15,40 +15,37 @@ L.Control.geocoder({
     geocoder: L.Control.Geocoder.nominatim()
 }).addTo(map);
 
+fetch("https://yashdb18-hersafety.hf.space/app/get_maps_data")
+    .then(async (res) => {
+        data = await res.json()
+        let addressPoints = JSON.parse(data.data)
+        console.log(addressPoints)
+        addressPoints = addressPoints.map(point => {
+            point[2] *= 100;
+            return point;
+        });
 
-var addressPoints = [
-    [12.2958, 76.6394, 100],  // Mysore Palace
-    [12.3072, 76.6552, 7],    // Chamundi Hill
-    [12.3150, 76.6400, 3],    // Mysore Zoo
-    [12.3012, 76.6382, 8],    // St. Philomena's Church
-    [12.3062, 76.6497, 200],  // Jaganmohan Palace
-    [12.2745, 76.6469, 50],   // Karanji Lake
-    [12.3125, 76.6700, 9],    // Lalitha Mahal Palace
-    [12.3071, 76.6383, 5],    // Devaraja Market
-    [12.3256, 76.6238, 6],    // Railway Museum
-    [12.3158, 76.6411, 500],  // Regional Museum
-    [12.2900, 76.6200, 3],    // Outer Ring Road
-    [12.3000, 76.6600, 7],    // Siddhartha Layout
-    [12.3100, 76.6300, 1],    // Nazarbad
-    [12.3200, 76.6450, 2],    // Gokulam
-    [12.2800, 76.6550, 500],  // Vidyaranyapuram
-    [12.3400, 76.6200, 8],    // Hebbal
-    [12.2700, 76.6400, 0],    // Alanahalli
-    [12.3050, 76.6750, 6],    // Bannimantap
-    [12.3000, 76.6500, 2],    // Chamundipuram
-    [12.2950, 76.6300, 4],    // Lakshmipuram
-];
+
+        let lessThan5 = addressPoints.filter(point => point[2] < 500);  // originally less than 5
+        let greaterOrEqual5 = addressPoints.filter(point => point[2] >= 500);
+        var heat1 = L.heatLayer(lessThan5, {
+            radius: 25,
+            blur: 15,
+            maxZoom: 17,
+            gradient: { 0.5: 'green' }
+        }).addTo(map);
+
+        var heat2 = L.heatLayer(greaterOrEqual5, {
+            radius: 30,
+            blur: 15,
+            maxZoom: 17,
+            gradient: { 0.5: 'red' }
+        }).addTo(map);
+    })
+
+    .catch((err) => {
+        console.log(err);
+    })
+
 
 // Create heatmap
-var heat = L.heatLayer(addressPoints, {
-    radius: 25,
-    blur: 15,
-    maxZoom: 17,
-    gradient: {
-        0.0: 'blue',
-        0.2: 'lime',
-        0.4: 'yellow',
-        0.6: 'orange',
-        1.0: 'red'
-    }
-}).addTo(map);
